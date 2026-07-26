463ab39c57cf1b7a87a61b66c07f91d39c370f53
Igor Nathan Lira <nathanlira15@gmail.com>
2026-07-26 01:02:48 -0300

    feat: adicionar itens e mudar somente quantidade, sem duplicar produto

--- script.js
+++ script.js
@@ -9,17 +9,50 @@ const cartCounter = document.getElementById("cart-count")
 const addressInput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
 
+let cart = [];
+
 //abrir o modal do carrinho
-cartBtn.addEventListener("click", function() {
+cartBtn.addEventListener("click", function () {
     cartModal.style.display = "flex"
 })
 
-cartModal.addEventListener("click" ,function(event) {
-    if(event.target === cartModal) {
+cartModal.addEventListener("click", function (event) {
+    if (event.target === cartModal) {
         cartModal.style.display = "none"
     }
 })
 
-closeModalBtn.addEventListener("click", function(){
+closeModalBtn.addEventListener("click", function () {
     cartModal.style.display = "none"
 })
+
+menu.addEventListener("click", function (event) {
+    //console.log(event.target)
+
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if (parentButton) {
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        //adicionar no carrinho
+        addToCart(name, price)
+    }
+})
+
+//função para adicionar no carrinho
+function addToCart(name, price) {
+    const existingItem = cart.find(item => item.name === name)
+
+    if (existingItem) {
+        //se o item ja existe, aumenta apenas a quantidade + 1
+
+        existingItem.quantity += 1;
+        return;
+    }
+    cart.push({
+        name,
+        price,
+        quantity: 1,
+    })
+}
